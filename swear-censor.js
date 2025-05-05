384363e3f120852b334bac03846ea79a409a9e59
Schuyler Tan (SLT World) <76160587+SLT-World@users.noreply.github.com>
2025-05-05 18:50:57 +0800

    Create swear-censor.js

---
+++ swear-censor.js
@@ -0,0 +1,12 @@
+const originalRenderMessageHTML = renderMessageHTML;
+
+const words = ["fuck", "shit", "bitch", "nigga", "nigger", "pussy", "bastard", "dick", "cunt", "piss", "crap", "slut", "whore", "faggot", "ass"];
+const regex = new RegExp(words.join('|'), 'i');
+
+renderMessageHTML = function(message) {
+  if (regex.test(message.text.replace(/\s/g, ''))) {
+    message.text = `<p class="spoiler" onclick="spoiler(this)">${message.text}</p>
+      <i class="bx bx-error-circle" style="font-size: 20px; position: relative; bottom: 2px; color: red; vertical-align: middle;"></i>`;
+  }
+  return originalRenderMessageHTML(message);
+};
