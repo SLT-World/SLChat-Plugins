659d57f9df609eb511ef8deb747b3d07a25481b5
Schuyler Tan (SLT World) <76160587+SLT-World@users.noreply.github.com>
2025-05-08 22:52:56 +0800

    Update sound-notification.js

--- sound-notification.js
+++ sound-notification.js
@@ -1,8 +1,10 @@
-const user_id = GetCookie("op");
-const notif_audio = new Audio('https://media.memesoundeffects.com/2021/06/Discord-Notification-Sound-Effect.mp3');
-
-socket.on('prompt', function(prompt) {
-  if (document.hidden && prompt.message.owner.id != user_id) {
-      notif_audio.play();
-  };
-});
+if (socket) {
+  const user_id = GetCookie("op");
+  const notif_audio = new Audio('https://media.memesoundeffects.com/2021/06/Discord-Notification-Sound-Effect.mp3');
+  
+  socket.on('prompt', function(prompt) {
+    if (document.hidden && prompt.message.owner.id != user_id) {
+        notif_audio.play();
+    };
+  });
+}
