e4449fa4aedfdf4792e3b1010e1b874bcfdf5908
Schuyler Tan (SLT World) <76160587+SLT-World@users.noreply.github.com>
2025-05-08 19:29:04 +0800

    Create pink-theme.js

---
+++ gurl-powah-theme.js
@@ -0,0 +1,5 @@
+document.documentElement.style.setProperty('--primary-color', 'darkslateblue');
+document.documentElement.style.setProperty('--darker-secondary-color', 'mediumpurple');
+document.documentElement.style.setProperty('--secondary-color', 'blueviolet');
+document.documentElement.style.setProperty('--light-gray-color', 'fuchsia');
+document.documentElement.style.setProperty('--gray-color', 'violet');
